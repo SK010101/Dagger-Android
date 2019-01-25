d731983d56e0c716a6dd080ebdbab08b133b44a8
shivam19980 <skshivamk1998@gmail.com>
2019-01-25 15:32:41 +0530

    commit

--- app/src/main/java/com/skshivamk/omen/advanceandroid/home/MainActivityComponent.java
+++ app/src/main/java/com/skshivamk/omen/advanceandroid/home/MainActivityComponent.java
@@ -17,7 +17,7 @@ public interface MainActivityComponent extends AndroidInjector<MainActivity> {
 
         @Override
         public void seedInstance(MainActivity instance) {
-
+            
         }
     }
 }
